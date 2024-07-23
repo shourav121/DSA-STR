bc2f24c42918a6b7ff9a0655e62c739a10cbfdc2
shourav kumar <shouravk81@gmail.com>
2024-07-23 15:21:49 +0530

    work

---
+++ ARRAY/PrintingAnArray.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[5]={3,7,5,4,1};
+    for(int i=0;i<=4;i++)
+    {
+        cout<<" "<<arr[i];
+    }
+}
---
+++ ARRAY/SyntaxOfArray.cpp
@@ -0,0 +1,21 @@
+/*#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[5]={3,5,1,8,5};
+    cout<<arr[2];
+    return 0;
+}*/
+
+ 
+                                       //update valude on array//                 
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[4]={8,7,3,5};
+    arr[2]=9;
+    cout<<arr[2];
+}
